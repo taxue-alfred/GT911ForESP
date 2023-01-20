db98de9580dbc9587d0cb34e7aeb0b1ad3329719
taxue-alfred <taxue_tianxing@outlook.com>
2023-01-20 18:45:07 +0800

    fix .gitignore

--- cmake-build-debug/CMakeFiles/git-data/grabRef.cmake
+++ cmake-build-debug/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "D:/Projects/PersonalProject/GT911ForESP/cmake-build-debug/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "D:/ESP-IDF/esp-idf/.git")
+set(GIT_DIR "D:/Projects/PersonalProject/GT911ForESP/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
